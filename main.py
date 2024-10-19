c7b3b10e419d010c986434464cc50c55fab56ecb
maupastor <maurizio.pastore98@gmail.com>
2024-10-20 00:05:18 +0200

    added counting of words in a file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt"
-    print(get_file_text(book_path))
+    text = get_file_text(book_path)
+    print(f"Word Count: {get_words_count(text)}")
 
 def get_file_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
+
+def get_words_count(text):
+    return len(text.split())
 
 main()
