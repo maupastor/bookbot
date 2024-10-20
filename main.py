91231b626d2a54144edda2e15d4576c91d80d22d
maupastor <maurizio.pastore98@gmail.com>
2024-10-20 21:57:09 +0200

    added report of word and char count

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 def main():
     book_path = "books/frankenstein.txt"
     txt = get_file_text(book_path)
-    print(f"Word Count: {get_words_count(txt)}")
-    print(f"Char dict: {get_chars_count(txt)}")
+    print_report(get_words_count(txt), get_chars_count(txt))
+
+def print_report(words_count, chars_count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words_count} words found in the document\n")
+
+    chars_list = get_sorted_list_by_count(chars_count)
+
+    for c in chars_list:
+        if c["char"].isalpha():
+            print(f"The '{c['char']}' character was found {c['count']} times")
+    
+    print("--- End report ---")
 
 def get_file_text(path):
     with open(path) as f:
@@ -19,5 +30,19 @@ def get_chars_count(txt):
         count = chars.get(lower_txt[i]) or 0
         chars[lower_txt[i]] = count + 1
     return chars
+
+def get_sorted_list_by_count(d):
+    ld = dic_to_list_of_dicts(d)
+    ld.sort(reverse=True, key=sort_by_count)
+    return ld
+
+def dic_to_list_of_dicts(d):
+    ld = []
+    for key in d:
+        ld.append({"char" : key, "count" : d[key]})
+    return ld
+
+def sort_by_count(d):
+    return d["count"]
 
 main()
